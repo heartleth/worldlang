ffe14d04b53d9b01454b6b2a7875cd3395d3d7fa
heartleth <62895113+heartleth@users.noreply.github.com>
2021-02-09 00:30:14 +0900

    input node.js
    ㅇㅇ

--- libs/worldlib.js
+++ libs/worldlib.js
@@ -1,18 +1,18 @@
-const readline = require("readline");
+const fs = require('fs');
 
 module.exports = {
     print: (...arg) => process.stdout.write(arg.reduce((a,b)=>a+b,'')),
     println: (...arg) => console.log(arg.reduce((a,b)=>a+b,'')),
     input: (q) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        const s = await new Promise(p=>rl.question(q, (s)=>{
-            p(s);
-        }));            
-        rl.close();
-        return s;
+        print(q);
+        let buf = Buffer.alloc(1);
+        for (;/^[\s\t\r\n]$/.test(buf.toString());fs.readSync(0,buf,0,1))buf=Buffer.alloc(1);
+        let ret = '';
+        for (;/^[\s\t\r\n]$/.test(buf.toString());fs.readSync(0,buf,0,1)){
+            ret+=buf.toString();
+            buf=Buffer.alloc(1);
+        }
+        return ret.substr(1).trim()
     }
     // ...Todo
 }
