b27d06f28954502eb0091426987dbfd11bbf1c7a
heartleth <62895113+heartleth@users.noreply.github.com>
2021-02-13 23:03:10 +0900

    delete duplicated code, Kotlin wip
    ㅇㅇ

--- src/runner/filesys.rs
+++ src/runner/filesys.rs
@@ -19,70 +19,35 @@ pub fn convert_to_cpp(path :&String, ext :&str, lang :&json::JsonValue)->std::io
     let mut source = String::new();
     source_file.read_to_string(&mut source)?;
     let converted :String = transpile(&tree::CodeTree::treeify(&source), 0, &lang);
-    if ext == lang["h_ext"].as_str().unwrap() {
-        unsafe {
-            if IS_FIRST {
-                colour::green!("  Transpiling ");
-            }
-            else {
-                colour::green!("=>Transpiling ");
-            }
+    unsafe {
+        if IS_FIRST {
+            colour::green!("  Transpiling ");
         }
-        println!("{0}.epp => {0}.{1}", path, ext);
-        target.write_all(render(&lang["header_guards"].as_str().unwrap(), &json!({
-            "token": rand::thread_rng().sample_iter(&Alphanumeric).take(20).collect::<String>(),
-            "contents": converted,
-            "filename": format!("{}.{}", path, ext),
-            "filenamewithoutext": path
-        })).unwrap().as_bytes())?;
-        
-        for c in lang["then"].members() {
-            let cmd = render(c.as_str().unwrap(), &json!({"file": path})).unwrap();
-            colour::green!("=>Executing ");
-            println!("{}", cmd);
-            if cfg!(windows) {
-                std::process::Command::new("cmd")
-                    .args(&["/c", &cmd]).output()
-                    .expect("Error!");
-            }
-            else {
-                std::process::Command::new("sh")
-                    .args(&["-c", &render(jpath!(lang, then).unwrap_or(""), &json!({"file": path})).unwrap_or(String::new())]).output()
-                    .expect("Error!");
-            }
+        else {
+            colour::green!("=>Transpiling ");
         }
     }
-    else {
-        unsafe {
-            if IS_FIRST {
-                colour::green!("  Transpiling ");
-            }
-            else {
-                colour::green!("=>Transpiling ");
-            }
+    println!("{0}.epp => {0}.{1}", path, ext);
+    target.write_all(render(&lang["header_guards"].as_str().unwrap(), &json!({
+        "token": rand::thread_rng().sample_iter(&Alphanumeric).take(20).collect::<String>(),
+        "contents": converted,
+        "filename": format!("{}.{}", path, ext),
+        "filenamewithoutext": path
+    })).unwrap().as_bytes())?;
+
+    for c in lang["then"].members() {
+        let cmd = render(c.as_str().unwrap(), &json!({"file": path})).unwrap();
+        colour::green!("=>Executing ");
+        println!("{}", cmd);
+        if cfg!(windows) {
+            std::process::Command::new("cmd")
+                .args(&["/c", &cmd]).output()
+                .expect("Error!");
         }
-        println!("{0}.epp => {0}.{1}", path, ext);
-        target.write_all(render(&lang["header_guards"].as_str().unwrap(), &json!({
-            "token": rand::thread_rng().sample_iter(&Alphanumeric).take(20).collect::<String>(),
-            "contents": converted,
-            "filename": format!("{}.{}", path, ext),
-            "filenamewithoutext": path
-        })).unwrap().as_bytes())?;
-        
-        for c in lang["then"].members() {
-            let cmd = render(c.as_str().unwrap(), &json!({"file": path})).unwrap();
-            colour::green!("=>Executing ");
-            println!("{}", cmd);
-            if cfg!(windows) {
-                std::process::Command::new("cmd")
-                    .args(&["/c", &cmd]).output()
-                    .expect("Error!");
-            }
-            else {
-                std::process::Command::new("sh")
-                    .args(&["-c", &render(jpath!(lang, then).unwrap_or(""), &json!({"file": path})).unwrap_or(String::new())]).output()
-                    .expect("Error!");
-            }
+        else {
+            std::process::Command::new("sh")
+                .args(&["-c", &render(jpath!(lang, then).unwrap_or(""), &json!({"file": path})).unwrap_or(String::new())]).output()
+                .expect("Error!");
         }
     }
     unsafe { IS_FIRST = false; }
